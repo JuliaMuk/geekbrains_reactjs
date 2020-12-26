1566d473a1cb18f64a377eed3fdd92b99bbd0484
JuliaMuk <mukhina_ur@mail.ru>
2020-12-26 20:20:21 +0500

    go

---
+++ static_src/components/App.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default App = (props) => {
+    return <h1>Просто текст</h1>;
+};
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const webpack = require('webpack');
+module.exports = {
+    entry: {
+        app: './static_src/index.jsx',
+    },
+   // context: path.resolve(__dirname, "static_src"),
+    output: {
+        path: path.resolve(__dirname, "static", "build"),
+        filename: 'app.js',
+        publicPath: '/static/build/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                include: path.resolve(__dirname, "static_src"),
+                exclude: path.resolve(__dirname, "node_modules"),
+                loader: 'babel-loader',                
+                options: {
+                    presets: ['@babel/env', '@babel/react'],
+                }
+            
+                    
+            },
+        ],
+    },
+   
+        
+        
+};
